84c3ca095390a0aaab332163cd97fe335ebbb949
liuxianglin <nearwind_x@163.com>
2017-11-30 22:38:53 +0800

    learn package

---
+++ basic-sytax/src/demo/PointDemo.java
@@ -0,0 +1,61 @@
+package demo;
+
+// 点对象
+class Point {
+	private int x; //横坐标
+	private int y; //纵坐标
+	
+	Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX(){
+		return x;
+	}
+	
+	public int getY(){
+		return y;
+	}
+}
+
+// 圆对象
+class Circle {
+	private int r; // 半径
+	Circle(int r) {
+		this.r = r;
+	}
+	
+	/*
+	 * 判断点和圆的关系，
+	 *     返回有三种结果
+	 * 	    1:表明在圆外
+	 * 	    0:表明在圆周上
+	 *     -1:表明在圆内
+	 */
+	int judge(Point p) {
+		int distance = p.getX() * p.getX() + p.getY() * p.getY();
+		int rr = this.r * this.r;
+		if (distance > rr ) {
+			return 1;
+		} else if (distance < rr) {
+			return -1;
+		} else {
+			return 0;
+		}
+	}
+}
+// 构造器
+public class PointDemo {
+	public static void main(String[] args){
+		// 创建一个点对象
+		Point p = new Point(3, 4);
+		
+		Circle c = new Circle(5);
+		
+		// 判断
+		int ret = c.judge(p);
+		
+		System.out.println(ret);
+	}
+}
--- basic-sytax/src/demo/ThisDemo.java
+++ basic-sytax/src/demo/ThisDemo.java
@@ -5,7 +5,7 @@ class User {
 	private String name;
 	private int age;
 	
-	public String getName(){
+	/*public String getName(){
 		return name;
 	}
 	
@@ -23,15 +23,35 @@ class User {
 			return;
 		}
 		this.age = age;
+	}*/
+	
+	/*
+	 * 定义构造器
+	 * 构造器重载时，用this()方法必须要写在代码块的第一行
+	 */
+	
+	User(String name){
+		this(name, 0);
+		System.out.println(this.name);
+	}
+	
+	User(String name, int age) {
+		this.name = name;
+	 	this.age = age;
 	}
 	
 	public void show(){
 		System.out.println(this.name + "今年" + this.age);
 	}
+	
+	public void sayHello(){
+		System.out.println("Hello");
+		this.show();
+	}
 }
 public class ThisDemo {
 	public static void main(String[] args) {
-		User u1 = new User();
+		/*User u1 = new User(); // 无参数构造器
 		u1.setName("小明");
 		u1.setAge(23);
 		String n = u1.getName();
@@ -40,6 +60,9 @@ public class ThisDemo {
 		User u2 = new User();
 		u2.setName("小王");
 		u2.setAge(33);
-		u2.show();
+		u2.show();*/
+		
+		User u3 = new User("小明");
+		u3.show();
 	}
 }
