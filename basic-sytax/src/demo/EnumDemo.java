5901246c3ec72966f81dbc951b48f6d9c96c049e
liuxianglin <nearwind_x@163.com>
2017-12-06 22:59:14 +0800

    learn enum

--- basic-sytax/src/demo/EnumDemo.java
+++ basic-sytax/src/demo/EnumDemo.java
@@ -1,6 +1,6 @@
 package demo;
 // 定义一个星期几的常量类
-class Weekday {
+/*class Weekday {
 	private Weekday(){}; // 私有化构造器
 	public static final Weekday MONDAY = new Weekday();
 	public static final Weekday TUESDAY = new Weekday();
@@ -9,11 +9,21 @@ class Weekday {
 	public static final Weekday FRIDAY = new Weekday();
 	public static final Weekday SATURDAY = new Weekday();
 	public static final Weekday SUNDAY = new Weekday();
-}
+}*/
 /*
  * 还是存在一个弊端，数据类型不安全，可以设置成[1,7]之外的数字
  */
-// 枚举类
+// 枚举类,最后一个要用分号
+enum Weekday {
+	MONDAY,
+	TUESDAY,
+	WEDNSDAY,
+	THURSDAY,
+	FRIDAY,
+	SATURDAY,
+	SUNDAY;
+}
+// 等价于class Weekday
 class Employees {
 	private Weekday restDay;//一周的哪一天休息
 	
@@ -35,6 +45,16 @@ public class EnumDemo {
 			System.out.println("周末休息");
 		} else {
 			System.out.println("周一到周五休息");
+		}
+		/*
+		 * 枚举类的一些使用方法
+		 */
+		System.out.println(Weekday.SUNDAY);
+		System.out.println(Weekday.SUNDAY.name()); // 返回常量名
+		System.out.println(Weekday.SUNDAY.ordinal()); // 返回下标
+		Weekday[] ws = Weekday.values(); // 获取所有的常量名
+		for(Weekday day : ws) {
+			System.out.println(day);
 		}
 	}
 }
