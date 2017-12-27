f15f62c08515e1656e4c589ae6c11c21e2c210db
liuxianglin <943572695@qq.com>
2017-12-27 23:20:17 +0800

    solve the versionid conflict

--- basic-sytax/src/com/superbx/io/serializable/ObjectStreamDemo.java
+++ basic-sytax/src/com/superbx/io/serializable/ObjectStreamDemo.java
@@ -1,9 +1,35 @@
 package com.superbx.io.serializable;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 /*
  * 使用对象流来实现序列化与反序列化
  */
 public class ObjectStreamDemo {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		File f = new File("file/ch.txt");
 		
+		//writeObject(f);
+		readObject(f);
+	}
+	
+	//序列化操作
+	private static void writeObject(File f) throws IOException, IOException {
+		ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(f));
+		out.writeObject(new User("lxl", "password", 23));
+		out.close();
+	}
+	//反序列化操作
+	private static void readObject(File f) throws Exception {
+		ObjectInputStream in = new ObjectInputStream(new FileInputStream(f));
+		User user = (User) in.readObject();
+		System.out.println(user);
+		in.close();
 	}
 }
--- basic-sytax/src/com/superbx/io/serializable/User.java
+++ basic-sytax/src/com/superbx/io/serializable/User.java
@@ -1,5 +1,25 @@
 package com.superbx.io.serializable;
 
-public class User {
+import java.io.Serializable;
 
+public class User implements Serializable {
+	//解决反序列化时版本号冲突的问题就是将版本号定义成常量
+	private static final long serialVersionUID = 1L;
+	private String name;
+	transient private String password; //当某一个参数不需要序列化的时候，用transient修饰符，表示瞬态的，就不会被序列化进去
+	private int age;
+	private String email;
+	
+	public User(String name, String password, int age) {
+		super();
+		this.name = name;
+		this.password = password;
+		this.age = age;
+	}
+
+	public String toString() {
+		return "User [name=" + name + ", password=" + password + ", age=" + age
+				+ "]";
+	}
+	
 }
